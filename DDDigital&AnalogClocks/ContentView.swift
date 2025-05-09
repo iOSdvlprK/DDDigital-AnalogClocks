1ba90578ddb6d6b525e19de28b717258dc80dba2
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-05-09 22:11:25 +0900

    Initial Commit

---
+++ DDDigital&AnalogClocks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DDDigital&AnalogClocks
+//
+//  Created by joe on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DDDigital&AnalogClocks/DDDigital_AnalogClocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDDigital_AnalogClocksApp.swift
+//  DDDigital&AnalogClocks
+//
+//  Created by joe on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct DDDigital_AnalogClocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
