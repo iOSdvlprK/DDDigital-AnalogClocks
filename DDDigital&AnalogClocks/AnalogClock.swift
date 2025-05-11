795305e2e503186a674fe94dfec0da3ad0545d91
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-05-11 22:43:54 +0900

    add the hands to the analog clock

--- DDDigital&AnalogClocks/AnalogClock.swift
+++ DDDigital&AnalogClocks/AnalogClock.swift
@@ -33,6 +33,30 @@ struct ClockView: View {
             let hours = calendar.component(.hour, from: date) % 12
             
             // angles for clock hands
+            func angle(for value: Int, unit: Int) -> Angle {
+                .degrees(Double(value) / Double(unit) * 360 - 90)
+            }
+            
+            // clock hands
+            let secondAngle = angle(for: seconds, unit: 60)
+            let minuteAngle = angle(for: minutes, unit: 60)
+            let hourAngle = angle(for: hours * 5 + minutes / 12, unit: 60)
+            
+            func hand(at angle: Angle, length: CGFloat, color: Color) {
+                let angleRadians: Double = angle.radians
+                var path = Path()
+                let end = CGPoint(
+                    x: center.x + cos(Double(angleRadians)) * length,
+                    y: center.y + sin(angleRadians) * length
+                )
+                path.move(to: center)
+                path.addLine(to: end)
+                context.stroke(path, with: .color(color), lineWidth: 2)
+            }
+            
+            hand(at: hourAngle, length: size.width * 0.25, color: .primary)
+            hand(at: minuteAngle, length: size.width * 0.35, color: .primary)
+            hand(at: secondAngle, length: size.width * 0.4, color: .red)
             
             // center dot
             let centerCircle = Path(
